f86bf209e5e8eda230a61fb4fff42fd99f19a616
Himanshu <hsdhimanshu41@gmail.com>
2026-07-19 22:08:07 +0530

    build(settings): configure project-level templates directory

--- ai_ops/settings/base.py
+++ ai_ops/settings/base.py
@@ -127,13 +127,16 @@ ROOT_URLCONF = 'ai_ops.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.debug",
             ],
         },
     },
