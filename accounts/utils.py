fde76eebb1545e471cf9ff6ddb95d80ea4ca0c89
Himanshu <hsdhimanshu41@gmail.com>
2026-07-25 21:23:50 +0530

    test(accounts) : debugging send email

--- accounts/utils.py
+++ accounts/utils.py
@@ -14,23 +14,39 @@ def generate_token():
 def hash_token(token: str) -> str:
     return hashlib.sha256(token.encode()).hexdigest()
 
-def send_email(subject: str,message: str,recipient_list: list[str],) -> None:
+# def send_email(subject: str,message: str,recipient_list: list[str],) -> None:
 
-    # This Will be Used in Production, For Now We Just Print the Email Content to the Console for Debugging
-    send_mail(subject=subject, message=message, 
-              from_email=settings.DEFAULT_FROM_EMAIL, 
-              recipient_list=recipient_list, fail_silently=False, 
-              timeout=20,
-            )
+#     # This Will be Used in Production, For Now We Just Print the Email Content to the Console for Debugging
+#     send_mail(subject=subject, message=message, 
+#               from_email=settings.DEFAULT_FROM_EMAIL, 
+#               recipient_list=recipient_list, fail_silently=False, 
+#               timeout=20,
+#             )
 
 
-    if settings.DEBUG:
-        # This is for the Development Environment, to see the email content in the console
-        logger.debug("\n========== RAW EMAIL ==========")
-        logger.debug("TO: %s", recipient_list)
-        logger.debug("SUBJECT: %s", subject)
-        logger.debug("MESSAGE: %s", message)
-        logger.debug("================================\n")
+#     if settings.DEBUG:
+#         # This is for the Development Environment, to see the email content in the console
+#         logger.debug("\n========== RAW EMAIL ==========")
+#         logger.debug("TO: %s", recipient_list)
+#         logger.debug("SUBJECT: %s", subject)
+#         logger.debug("MESSAGE: %s", message)
+#         logger.debug("================================\n")
+
+
+def send_email(subject, message, recipient_list):
+
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=recipient_list,
+            fail_silently=False,
+        )
+
+    except Exception:
+        logger.exception("EMAIL SENDING FAILED")
+        raise
 
 
 
