a2fc7e406a29c3de3f6a5a2ead8f1998bab0e342
Himanshu <hsdhimanshu41@gmail.com>
2026-08-04 15:21:08 +0530

    test(factory): add UserFactory

--- tests/factories/user_factory.py
+++ tests/factories/user_factory.py
@@ -1,39 +1,22 @@
 import factory
+from factory.django import DjangoModelFactory
 from django.contrib.auth import get_user_model
-
 
 User = get_user_model()
 
 
-class UserFactory(factory.django.DjangoModelFactory):
-    """
-    Factory for the custom User model.
-
-    Produces realistic users for:
-    - authentication tests
-    - service ownership
-    - permissions
-    - integration tests
-    """
-
+class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
         django_get_or_create = ("email",)
 
     email = factory.Sequence(lambda n: f"user{n}@example.com")
-
     first_name = factory.Faker("first_name")
-
-    is_active = True
-    is_verified = True
-    is_staff = False
-    is_superuser = False
-
-    auth_provider = "local"
-
-    provider_id = None
 
     password = factory.PostGenerationMethodCall(
         "set_password",
-        "TestPassword@123",
+        "Password@123",
     )
+
+    is_verified = True
+    is_active = True
